80039afedaa6c3d9ef85a6023424ac0435eb6207
Eddie Corrigall <eddiecorrigall@gmail.com>
2015-08-14 17:25:39 -0400

    Initial commit of setShareFormulaColumns.

--- Sheet.js
+++ Sheet.js
@@ -130,4 +130,38 @@ Sheet.prototype.setColumnValues = function (row, columnValues) {
     }
 };
 
+/**
+ Given a list of shared formula addresses, extend the formula along the column for a given number of rows.
+ @ param {integer} numberOfRows
+ @ param {array} sharedFormulaAddresses
+ */
+Sheet.prototype.setShareFormulaColumns = function (numberOfRows, sharedFormulaAddresses) {
+    // Example:
+    // sheet.setShareFormulaColumns(433, ['F2', 'G2', 'H2', 'I2', 'J2', 'K2']);
+    if (!sharedFormulaAddresses) {
+        throw new Error('setShareFormulaColumns - requires sharedFormulaAddresses');
+    }
+    if ((utils.isInteger(numberOfRows) && (0 < numberOfRows)) === false) {
+        throw new Error('setShareFormulaColumns - numberOfRows - must be a positive integer');
+    }
+    for (var a = 0; a < sharedFormulaAddresses.length; a++) {
+        var sharedFormulaAddress = sharedFormulaAddresses[a];
+        var sharedFormulaCell = this.getCell(sharedFormulaAddress);
+        if (!sharedFormulaCell.isSharedFormula()) {
+            console.log(JSON.stringify(sharedFormulaCell._cellNode));
+            throw new Error('setShareFormulaColumns - not a shared formula');
+        }
+        var fNode = sharedFormulaCell._cellNode.find('./f');
+        var sharedIndex = fNode.get('si');
+        var lastSharedFormulaCell = null;
+        for (var r = 1; r < numberOfRows; r++) {
+            var lastSharedFormulaCell = sharedFormulaCell.getRelativeCell(r, 0);
+            lastSharedFormulaCell.setFormula(null, null, sharedIndex);
+        }
+        var range = [sharedFormulaAddress, lastSharedFormulaCell.getAddress()];
+        var ref = range.join(':');
+        fNode.set('ref', ref);
+    }
+};
+
 module.exports = Sheet;
