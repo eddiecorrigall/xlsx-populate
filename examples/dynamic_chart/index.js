7fff36e17e4f0d0fceb7d577d96b617fd2eacea2
Eddie Corrigall <eddiecorrigall@gmail.com>
2015-11-23 15:15:49 -0500

    Added comments and simplified code

--- examples/dynamic_chart/index.js
+++ examples/dynamic_chart/index.js
@@ -2,22 +2,30 @@
 
 var Workbook = require('../../');
 
+// Get workbook and sheet
 var workbook = Workbook.fromFileSync(__dirname + '/template.xlsx');
 var sheet = workbook.getSheet('ClickThroughRateSheet');
 
-var clicks = sheet.getCell('B3');
-var impressions = sheet.getCell('C3');
-var ctr = sheet.getCell('D3');
+// Get header cells
+var clicksHeader = sheet.getCell('B2');
+var impressionsHeader = sheet.getCell('C2');
+var ctrHeader = sheet.getCell('D2');
 
+// Randomly generate 10 rows of data
 var r = 0;
 while (r < 10) {
+	r++; // Skip header
 	var clickValue = parseInt(1000*Math.random());
 	var impressionValue = parseInt(1000000*Math.random());
-	clicks.getRelativeCell(r, 0).setValue(clickValue);
-	impressions.getRelativeCell(r, 0).setValue(impressionValue);
-	r++;
+	clicksHeader.getRelativeCell(r, 0).setValue(clickValue);
+	impressionsHeader.getRelativeCell(r, 0).setValue(impressionValue);
 }
 
-ctr.shareFormula(ctr.getRelativeCell(r-1, 0));
+// Assign shared formulas from the first 
+ctrHeader
+	.getRelativeCell(1, 0) // Start from the first cell below header
+	.shareFormula(ctrHeader.getRelativeCell(r, 0)) // End at the last modifed row
+	;
 
+// Save to file
 workbook.toFileSync(__dirname + '/out.xlsx');
