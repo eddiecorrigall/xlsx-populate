73d05e69b82d0454e6d0c7bc6cdd821c9683f469
Eddie Corrigall <eddiecorrigall@gmail.com>
2015-12-24 19:50:36 -0500

    Coverted scripts to gulp

---
+++ gulpfile.js
@@ -0,0 +1,47 @@
+var gulp = require('gulp');
+var watch = require('gulp-watch');
+var cached = require('gulp-cached');
+var jshint = require('gulp-jshint');
+var jshintStylish = require('jshint-stylish');
+var jscs = require('gulp-jscs');
+var jscsStylish = require('jscs-stylish');
+var jasmine = require('gulp-jasmine');
+
+var LIB = 'lib/*.js';
+var TEST = 'test/*.js';
+var SRC = [LIB, TEST];
+
+gulp.task('jshint', function () { // linting
+	return gulp
+		.src(SRC)
+		.pipe(cached('jshint-linting'))
+		.pipe(jshint({ lookup: true }))
+		//.pipe(jshint.reporter('default'))
+		.pipe(jshint.reporter(jshintStylish))
+		.pipe(jshint.reporter('fail'))
+		;
+});
+
+gulp.task('jscs', function () { // linting
+	return gulp
+		.src(SRC)
+		.pipe(cached('jscs-linting'))
+		.pipe(jscs())
+        .pipe(jscs.reporter(jscsStylish))
+        .pipe(jscs.reporter('fail'))
+        ;
+});
+
+gulp.task('test', ['jshint', 'jscs'], function () {
+	return gulp
+		.src(TEST)
+		.pipe(cached('testing'))
+		.pipe(jasmine())
+		;
+});
+
+gulp.task('watch', function () {
+	gulp.watch([SRC, TEST], ['test']);
+});
+
+gulp.task('default', ['watch']);
