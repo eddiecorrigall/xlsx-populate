6684413649fb13e690080b8130fc6319bbc6a5ad
Eddie Corrigall <eddiecorrigall@gmail.com>
2015-12-25 12:05:17 -0500

    Applied linting changes

--- examples/dynamic_chart/index.js
+++ examples/dynamic_chart/index.js
@@ -14,14 +14,13 @@ var ctrHeader = sheet.getCell('D2');
 // Randomly generate 10 rows of data
 var r = 0;
 while (r < 10) {
-	
-	r++; // Skip header
+    r++; // Skip header
+    
+    var clickValue = parseInt(1e3 * Math.random());
+    var impressionValue = parseInt(1e6 * Math.random());
 
-	var clickValue = parseInt(1000*Math.random());
-	var impressionValue = parseInt(1000000*Math.random());
-
-	clicksHeader.getRelativeCell(r, 0).setValue(clickValue);
-	impressionsHeader.getRelativeCell(r, 0).setValue(impressionValue);
+    clicksHeader.getRelativeCell(r, 0).setValue(clickValue);
+    impressionsHeader.getRelativeCell(r, 0).setValue(impressionValue);
 }
 
 // Assign shared formulas
